ae1665e78e68bc1b5935319f4a67b00e49f08cee
Yash59ops <yashumehra18@gmail.com>
2024-01-01 21:17:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+const userDataRoute = require("./routes/userDataRoute");
+const cors=require('cors');
+app.use(cors());
+app.use(express.json());
+
+//Connect to mongodb database(locally)
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log("Connected Successfully");
+    app.listen(process.env.PORT || 5000, (err) => {
+      if (err) console.log(err);
+      console.log(`running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log("Failed to connect", error));
+
+app.use(userDataRoute);
